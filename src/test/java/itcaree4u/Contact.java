a5b50f9d51616bcc9df21e9749a4146cc3373720
aman kumar <eraman.kush@gmail.com>
2024-03-06 13:52:26 +0530

    inside master

--- src/test/java/itcaree4u/Contact.java
+++ src/test/java/itcaree4u/Contact.java
@@ -23,7 +23,6 @@ public class Contact {
 
 	@Test
 	public void TestContact() {
-		System.out.println("Title First:"+driver.getTitle()+":"+driver.getPageSource());
 		System.out.println("Title First:"+driver.getTitle()+":"+driver.getCurrentUrl());
 
 	}
